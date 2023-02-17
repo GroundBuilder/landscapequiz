0b4bf1a785b2a1f87e9014929f0df2baf47f7796
GroundBuilder <erik_rynnes@hotmail.com>
2023-02-17 11:03:06 +0000

    Add event listener for documentation loadin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,49 @@
 console.log("Connected to the index.html")
+// Wait for the DOM to finish loading before running the game.
+//Get the answer button elements nad add event listeners to them.
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", funtion(){
+            if (this.gettAttribute("data-type") === "submit"){
+                alert("You have klicked on a button");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+} ) 
+
+function runGame() {
+
+}
+
+function checkAnswers() {
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWorngAnswer() {
+
+}
+
+function incrementProgressBar(){
+
+}
+
+function displayQuizQuestion() {
+
+}
+
+function restarButton() {
+
+}
+
 
 // Creat an array to hold the picture and qusetion and answers.
