a46189d77b9fda2fa6d6c382c18e300e115d2351
GroundBuilder <erik_rynnes@hotmail.com>
2023-02-21 14:30:18 +0000

    Docs. Add modals, Style. simplify boxes

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Landscape quiz of shields ![Logo](assets/images/logo.png)
 
 **Landscape quiz** is a quiz game over the shields representaiting every landscape in Sweden. Every landscape has it own unique shield. 
+
+Credits
+
+How to creat Modals
+https://www.w3schools.com/howto/howto_css_modals.asp
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 console.log("Connected to the index.html")
 // Wait for the DOM to finish loading before running the game.
-//Get the answer button elements nad add event listeners to them.
+//Get the answer button elements and add event listeners to them.
 
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("buttons");
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
