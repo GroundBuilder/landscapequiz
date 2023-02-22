290b348d56821cd5a79d1e3cb82a4a4a307eeabd
GroundBuilder <erik_rynnes@hotmail.com>
2023-02-22 23:14:43 +0000

    Feat. more question and answers added

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,12 +97,127 @@ function optionResponse (respond) {
 let scenarios = [
     {
         id: 1,
-        question: "Witch landscape does this shield belong to?", 
+        question: "Which landscape does this shield belong to?", 
         background: "url('../images/upplands_vapen.png')",
         response: [
             {
                 option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
                 getTo: 1
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 1
+            },
+        ],
+    },
+    {
+        id: 2,
+        question: 'Which landscape does this shield belong to?',
+        background: "url('../images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 1
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 1
+            },
+            {
+                option: "Norrbotten",
+                getTo: 1
+            },
+        ],
+    },
+    {
+        id: 3,
+        question: 'Which landscape does this shield belong to?',
+        background: "url('../images/angermanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 1
+            },
+            {
+                option: "Vastmanland",
+                getTo: 1
+            },
+            {
+                option: "Angermanland",
+                getTo: 4
+            },
+            {
+                option: "Norrbotten",
+                getTo: 1
+            },
+        ],
+    },
+    {
+        id: 4,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('../images/blekinges_vapen.png')",
+        response: [
+            {
+                option: "Blekinge",
+                getTo: 5
+            },
+            {
+                option: "Vastmanland",
+                getTo: 1
+            },
+            {
+                option: "Uppland",
+                getTo: 1
+            },
+            {
+                option: "Norrbotten",
+                getTo: 1
+            },
+        ],
+    },
+    {
+        id: 5,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('../images/bohuslans_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 1
+            },
+            {
+                option: "Vastmanland",
+                getTo: 1
+            },
+            {
+                option: "Uppland",
+                getTo: 1
+            },
+            {
+                option: "Bohuslan",
+                getTo: 6
+            },
+        ],
+    },
+    {
+        id: 6,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
             },
             {
                 option: "Vastmanland",
@@ -119,8 +234,422 @@ let scenarios = [
         ],
     },
     {
-        id: 2,
-        question: `Witch landscape does this shield belong to?`,
+        id: 7,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 8,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 9,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 10,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 11,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 12,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 13,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 14,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 15,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 16,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 17,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 18,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 19,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 20,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 21,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 22,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 23,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 24,
+        question: `Which landscape does this shield belong to?`,
+        background: "url('assets/images/vastmanlands_vapen.png')",
+        response: [
+            {
+                option: "Gastrikland",
+                getTo: 2
+            },
+            {
+                option: "Vastmanland",
+                getTo: 3
+            },
+            {
+                option: "Uppland",
+                getTo: 2
+            },
+            {
+                option: "Norrbotten",
+                getTo: 3
+            },
+        ],
+    },
+    {
+        id: 25,
+        question: `Which landscape does this shield belong to?`,
         background: "url('assets/images/vastmanlands_vapen.png')",
         response: [
             {
