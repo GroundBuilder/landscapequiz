be424d958358677aabc5a35ab626ae47a84d16fa
GroundBuilder <erik_rynnes@hotmail.com>
2023-02-22 13:08:40 +0000

    Open and close modals is now working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-/*console.log("Connected to the index.html")
+/*
+console.log("Connected to the index.html")
 // Wait for the DOM to finish loading before running the game.
 //Get the answer button elements and add event listeners to them.
 
@@ -15,19 +16,35 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
-});*/
-// --  w3school modal
-let beginning = document.getElementsByClassName("quiz_info_box");
-let btnOpenRules = document.getElementsByClassName("beginning");
-let viewRules = document.getElementsByClassName("quiz_rule_info")[0];
+}); */
 
+// --  w3school modal
+// Get the modal
+let beginning = document.getElementById("quiz_info_box");
+
+// Get the button that opens the modal
+let btnOpenRules = document.getElementById("open_rules_btn");
+
+// Get the <span> element that closes the modal
+/*let viewRules = document.getElementsByClassName("quiz_rules_info")[0];*/
+let closeWindow = document.getElementById("start_btn_quiz");
+
+// When the user clicks the button, open the modal 
 btnOpenRules.onclick = function() {
     beginning.style.display = "block";
 }
 
-viewRules.onclick = function() {
+// When the user clicks on Begin Quiz button(x), close the modal
+closeWindow.onclick = function() {
     beginning.style.display = "none";
-}
+} 
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == game_background) {
+        quiz_info_box.style.display = "none";
+    }
+  } 
 
 
 
@@ -50,7 +67,7 @@ function checkAnswers() {
 
 }
 
-function incrementScore(){
+function incrementScore() {
 
 }
 
@@ -58,7 +75,7 @@ function incrementWorngAnswer() {
 
 }
 
-function incrementProgressBar(){
+function incrementProgressBar() {
 
 }
 
