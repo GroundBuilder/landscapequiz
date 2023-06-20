950d547a397964c5619c4f869a1a72409ab1caf9
GroundBuilder <erik_rynnes@hotmail.com>
2023-06-20 22:50:51 +0000

    Fix. add .js to import scenarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
 }); */
 
 
-const { scenarios } = require('./scenarios');
+import { scenarios } from "./scenarios.js";
 
 let quizText = document.getElementById("game_box");
 
